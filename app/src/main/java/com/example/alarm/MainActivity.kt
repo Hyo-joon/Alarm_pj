3e9234ebd4db1222059ceaa0f7f555e122d01710
Hyojoon Kim <guwns7732@gmail.com>
2022-07-23 00:08:47 +0900

    Alarm_main

--- app/src/main/java/com/example/alarm/MainActivity.kt
+++ app/src/main/java/com/example/alarm/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.alarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.NumberPicker
+import com.example.alarm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.timeHour.minValue = 0
+        binding.timeHour.maxValue = 12
+
+        binding.timeMinute.maxValue= 59
+        binding.timeMinute.minValue = 0
+
+        binding.timeAmpm.maxValue=1
+        binding.timeAmpm.minValue=0
+
     }
 }
